11d2ca3175deb5599f2dc151f078343f9e28570e
Jonathan S <37553600+iohnatan@users.noreply.github.com>
2024-02-14 19:05:35 -0300

    Update action.py

--- action.py
+++ action.py
@@ -12,10 +12,17 @@ def compute_checksum(file_path: str):
     return sha256_hash.hexdigest()
 
 
-def main(pattern: str, suffix: str):
+def main(pattern: str, suffix: str, subfolder: str):
     for filename in glob.glob(pattern):
         parent, base = os.path.split(filename)
         checksum = compute_checksum(filename)
+
+        parent = os.path.join(parent, subfolder)
+        if not os.path.exists(parent):
+             os.mkdir(parent)
+        elif not os.path.isdir(parent):
+             raise Exception("Subfolder already exist but is not a folder.")
+        
         with open(os.path.join(parent, f"{base}.{suffix}"), "w") as f:
             f.write(checksum)
 
@@ -30,6 +37,9 @@ if __name__ == "__main__":
     parser.add_argument(
         "--suffix", default="checksum", help="Suffix for the checksum files"
     )
+    parser.add_argument(
+        "--subfolder", required=False, help="Subfolder to put the checksum files"
+    )
 
     args = parser.parse_args()
-    main(pattern=args.pattern, suffix=args.suffix)
+    main(pattern=args.pattern, suffix=args.suffix, subfolder=args.subfolder)
